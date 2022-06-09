304dc31c103c99302b9d89c1678cbca0f833264c
Dani Lopez-Cruz <roberto.lc2493@gmail.com>
2022-06-09 00:26:00 -0600

    Add the completed comparison.js file

--- comparisons.js
+++ comparisons.js
@@ -20,25 +20,33 @@ var numberDogs = 0
 console.log("Is numberTeachers greater than numberStudents?", numberTeachers > numberStudents);
 // this should log: "Is numberTeachers greater than numberStudents?" false
 
+
 // YOU DO: log the result of the comparison: is numberTeachers less than numberStudents?
+console.log("is numberTeachers less than numberStudents? ", numberTeachers < numberStudents);
 // this should log: true
 
 // YOU DO: log the result of the comparison: is numberTeachers strictly equal to stringTeachers?
+console.log("is numberTeachers strictly equal to stringTeachers?", numberTeachers === stringTeachers);
 // this should log: false
 
 // YOU DO: log the result of the comparison: is numberTeachers not equal to numberStudents?
+console.log("is numberTeachers not equal to numberStudents?", numberTeachers !== numberStudents);
 // this should log: true
 
 // YOU DO: log the result of the comparison: is numberStudents greater than or equal to 20?
+console.log("is numberStudents greater than or equal to 20?", numberStudents >= 20);
 // this should log: true
 
 // YOU DO: log the result of the comparison: is numberStudents greater than or equal to 21?
+console.log("is numberStudents greater than or equal to 21?", numberStudents >= 21);
 // this should log: false
 
 // YOU DO: log the result of the comparison: is numberStudents less than or equal to 20?
+console.log("is numberStudents less than or equal to 20?", numberStudents <= 20);
 // this should log: true
 
 // YOU DO: log the result of the comparison: is numberStudents less than or equal to 21?
+console.log("is numberStudents less than or equal to 21?", numberStudents <= 21);
 // this should log: true
 
 
@@ -52,21 +60,27 @@ console.log("Is numberTeachers greater than numberStudents?", numberTeachers > n
 // Make sure YOU can explain it that way!
 
 console.log(4 < 9);
-// YOU DO: Explain.
+// this will log the comparison of whether 4 is less than 9
+// this will log true.
 
 var books = 3;
 console.log(4 < books);
-// YOU DO: Explain.
+// This will compare whether 4 is less than the variable book, which stores the value of 3
+// this will log false
 
 var friends = 6;
 var siblings = 2;
 console.log(friends > siblings);
-// YOU DO: Explain.
+// this will compare whether the variable friends (which stores the int value of 6)
+//is greater than the variable siblings (which stores the int value of 2)
+//this will log true
 
 var attendees = 9;
 var meals = 8;
 console.log(attendees != meals);
-// YOU DO: Explain.
+//this will compare whether the variable attendees (which stores the int value of 9)
+//is not equal to the variable meals (which store the int value of 8)
+//this will log true
 
 
 // #-------------------
@@ -89,15 +103,17 @@ var age = 1;
 
 // YOU DO:
 // Determine if the dog loves to play and loves treats
-
+console.log(lovesToPlay && lovesTreats);
 
 // Determine if the dog loves to play and loves the dog park
-
+console.log(lovesToPlay && lovesDogPark);
 
 // Determine if the dog loves to play or loves the dog park
-
+console.log(lovesToPlay || lovesDogPark);
 
 // Determine if the dog loves to play and is a puppy
-
+console.log(lovesToPlay && age)
 // What did your final line of code evaluate to? Why do you think that is? Explain.
-// ANSWER:
+// ANSWER: that final line of code came back as a string of 1, because this comparison
+//did not compare to another boolean, but an integer, i read that when compareds to a numberDogs
+// the comparison will come back as a string.
